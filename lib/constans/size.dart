dcb94e677c415c76e008fa9fd2d6448c95c206a1
MohsenRahmdeli <mohsen.rahmdeli@gmail.com>
2025-01-05 05:00:59 +0400

    create Responsive mobile and Desktop

---
+++ lib/constans/size.dart
@@ -0,0 +1,1 @@
+const kMinDesktopWidth = 600.0;
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../constans/color.dart';
 import '../constans/nav_items.dart';
+import '../constans/size.dart';
 import '../styles/style.dart';
 import '../widgets/drawer_mobile.dart';
 import '../widgets/header_desktop.dart';
@@ -14,42 +15,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: CustomColor.scaffoldBg,
-      endDrawer: DrawerMobile(),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          //Main
-          HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: () {},
-            onMenuTap: () {
-              scaffoldKey.currentState!.openEndDrawer();
-            },
-          ),
-          //Skill
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-          Container(
-            height: 500,
-            width: double.maxFinite,
-          ),
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-          Container(
-            height: 500,
-            width: double.maxFinite,
-          ),
-        ],
-      ),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        key: scaffoldKey,
+        backgroundColor: CustomColor.scaffoldBg,
+        endDrawer: constraints.maxWidth >= kMinDesktopWidth ? null : DrawerMobile(),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            //Main
+            if (constraints.maxWidth >= kMinDesktopWidth)
+              HeaderDesktop()
+            else
+              HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState!.openEndDrawer();
+                },
+              ),
+            //Skill
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
